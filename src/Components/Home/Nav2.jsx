bf3a2394a8b132167e4ac6e6817718f740ea5846
Anshika Agarwal <anshikaagarwal56890@gmail.com>
2025-08-05 15:58:50 +0530

    refactor: replace anchor tags with Link components for better navigation

--- src/Components/Home/Nav2.jsx
+++ src/Components/Home/Nav2.jsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import FaqSectionOne from "../FAQ/Sectionone";
 import ScrollToTopButton from "../ScrolltoTop";
+import { Link } from "react-router-dom";
 
 function Nav2() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -46,75 +47,75 @@ function Nav2() {
             {/* Desktop Nav Items */}
             <div className="hidden sm:flex flex-1 items-center justify-center">
               <div className="flex space-x-4 items-center">
-                <a
+                <Link
                   href="/"
                   className="rounded-md px-3 py-2 text-sm font-medium text-black hover:text-white hover:bg-gray-700"
                 >
                   Home
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                 >
                   Find Doctors
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/FAQ"
                   className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                 >
                   LabTests
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                 >
                   Shop
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                 >
                   Forum
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                 >
                   About Us
-                </a>
+                </Link>
 
                 {/* Icons */}
-                <a
+                <Link
                   href="#"
                   className="rounded-full px-3 py-2 text-sm font-medium text-white bg-gray-400 outline-green-600 ml-2"
                   aria-label="Add to bag"
                 >
                   <BsFillBagPlusFill />
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-full px-3 py-2 text-sm font-medium text-white bg-gray-400 outline-green-600 ml-2"
                   aria-label="Shopping cart"
                 >
                   <HiShoppingCart />
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-full px-3 py-2 text-sm font-medium text-white bg-gray-400 outline-green-600 ml-2"
                   aria-label="Notifications"
                 >
                   <IoIosNotifications />
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-full px-3 py-2 text-sm font-medium text-white bg-gray-400 outline-green-600 ml-2"
                   aria-label="Leaves"
                 >
                   <GiThreeLeaves />
-                </a>
-                <a href="#" className="text-black mt-2" aria-label="Dropdown">
+                </Link>
+                <Link href="#" className="text-black mt-2" aria-label="Dropdown">
                   <IoIosArrowDropdownCircle />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -122,78 +123,78 @@ function Nav2() {
           {/* Mobile Menu */}
           {menuOpen && (
             <div className="sm:hidden flex flex-col space-y-2 mt-4 px-4 pb-4">
-              <a
+              <Link
                 href="/"
                 className="text-black px-3 py-2 rounded-md text-sm hover:bg-gray-200"
               >
                 Home
-              </a>
-              <a
+              </Link>
+              <Link
                 href="#"
                 className="text-black px-3 py-2 rounded-md text-sm hover:bg-gray-200"
               >
                 Find Doctors
-              </a>
-              <a
+              </Link>
+              <Link
                 href="/FAQ"
                 className="text-black px-3 py-2 rounded-md text-sm hover:bg-gray-200"
               >
                 LabTests
-              </a>
-              <a
+              </Link>
+              <Link
                 href="#"
                 className="text-black px-3 py-2 rounded-md text-sm hover:bg-gray-200"
               >
                 Shop
-              </a>
-              <a
+              </Link>
+              <Link
                 href="#"
                 className="text-black px-3 py-2 rounded-md text-sm hover:bg-gray-200"
               >
                 Forum
-              </a>
-              <a
+              </Link>
+              <Link
                 href="#"
                 className="text-black px-3 py-2 rounded-md text-sm hover:bg-gray-200"
               >
                 About Us
-              </a>
+              </Link>
               <div className="flex gap-2 mt-2">
-                <a
+                <Link
                   href="#"
                   className="p-2 rounded-full bg-gray-400 text-white"
                   aria-label="Add to bag"
                 >
                   <BsFillBagPlusFill />
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="p-2 rounded-full bg-gray-400 text-white"
                   aria-label="Shopping cart"
                 >
                   <HiShoppingCart />
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="p-2 rounded-full bg-gray-400 text-white"
                   aria-label="Notifications"
                 >
                   <IoIosNotifications />
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="p-2 rounded-full bg-gray-400 text-white"
                   aria-label="Leaves"
                 >
                   <GiThreeLeaves />
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="mt-1 text-black"
                   aria-label="Dropdown"
                 >
                   <IoIosArrowDropdownCircle />
-                </a>
+                </Link>
               </div>
             </div>
           )}
